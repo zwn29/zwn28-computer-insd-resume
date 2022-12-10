a627ce0f525b4edd20f1e443696f437f68828d39
zenf <78023279+zencodee@users.noreply.github.com>
2022-12-10 07:54:27 +0530

    first commmit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Canvas } from '@react-three/fiber'
+import Experiance from './Experiance'
+
+function App() {
+  return (
+    <>
+      <Canvas>
+        <Experiance />
+      </Canvas>
+    </>
+  )
+}
+
+export default App
---
+++ src/Experiance.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect } from 'react'
+import font from './assets/bangers-v20-latin-regular.woff'
+import {
+  Text,
+  Html,
+  ContactShadows,
+  Float,
+  PresentationControls,
+  useGLTF,
+  Environment,
+} from '@react-three/drei'
+
+export default function Experiance() {
+  const computer = useGLTF(
+    'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf'
+  )
+  return (
+    <>
+      <Environment preset='city' />
+
+      <color args={['#012349']} attach='background' />
+
+      <PresentationControls
+        global
+        rotation={[0.2, 0.5, -0.1]}
+        polar={[-0.4, 0.2]}
+        azimuth={[-1, 0.75]}
+        config={{ mass: 2, tension: 400 }}
+        snap={{ mass: 4, tension: 400 }}
+      >
+        <Float rotationIntensity={0.4}>
+          <rectAreaLight
+            width={2.5}
+            height={1.65}
+            intensity={70}
+            color={'#375e89'}
+            rotation={[0.1, Math.PI, 0]}
+            position={[0, 0.55, -1.15]}
+          />
+          <primitive object={computer.scene} position-y={-1.2}>
+            <Html
+              transform
+              wrapperClass='htmlScreen'
+              distanceFactor={1.17}
+              position={[0, 1.56, -1.4]}
+              rotation-x={-0.256}
+            >
+              <iframe src='https://zwn28.online' />
+            </Html>
+          </primitive>
+          <Text
+            font={font}
+            fontSize={1}
+            position={[2, 0.75, 0.75]}
+            rotation-y={-1.25}
+            maxWidth={2}
+            textAlign='center'
+          >
+            zwn28
+          </Text>
+        </Float>
+      </PresentationControls>
+      <ContactShadows position-y={-1.4} opacity={0.4} scale={5} blur={2.4} />
+    </>
+  )
+}
